96f51527942407b66c4c310991ff129e91a859d0
Young Jun Park <syyjbhjh7@gmail.com>
2022-11-21 19:15:04 +0900

    Update : H2, hibernate관련 설정 추가, 동작 확인

--- src/main/java/com/jpa/api/ApiApplication.java
+++ src/main/java/com/jpa/api/ApiApplication.java
@@ -1,13 +1,19 @@
 package com.jpa.api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class ApiApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+		//SpringApplication.run(ApiApplication.class, args);
+		EntityManagerFactory emf =  Persistence.createEntityManagerFactory("practice");
+		EntityManager em = emf.createEntityManager();
 
+		em.close();
+		emf.close();
+	}
 }
